844bd0e00f2e5330eff0ac3ba99858e24c55df29
Artem <temaspsxxx@gmail.com>
2020-05-17 22:39:11 +0300

    Добавил показ бургер меню при малой ширине экрана и закрытие попапа при клике вне его

--- script/script.js
+++ script/script.js
@@ -2,13 +2,36 @@
 
 document.addEventListener('DOMContentLoaded', () => {
   const btnOpenModal = document.querySelector('#btnOpenModal');
+  const modalWrap = document.querySelector('.modal');
   const modalBlock = document.querySelector('#modalBlock');
   const closeModal = document.querySelector('#closeModal');
   const questionTitle = document.querySelector('#question');
   const formAnswers = document.querySelector('#formAnswers');
-  const burgerName = 'Стандарт';
-  const burgerImageSrc = './image/burger.png';
+  const burgerBtn = document.getElementById('burger');
 
+  let clientWidth = document.documentElement.clientWidth;
+
+  const showBurger = () => {
+    if (clientWidth < 768) {
+      burgerBtn.style.display = 'flex';
+    } else {
+      burgerBtn.style.display = 'none';
+    }
+  }
+
+  showBurger();
+
+  window.addEventListener('resize', () => {
+    clientWidth = document.documentElement.clientWidth;
+
+    showBurger();
+  });
+
+  burgerBtn.addEventListener('click', () => {
+    burgerBtn.classList.toggle('active');
+    modalBlock.classList.add('d-block');
+    playTest();
+  });
 
   btnOpenModal.addEventListener('click', () => {
     modalBlock.classList.add('d-block');
@@ -17,10 +40,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
   closeModal.addEventListener('click', () => {
     modalBlock.classList.remove('d-block');
+    burgerBtn.classList.remove('active');
+  });
+
+  modalWrap.addEventListener('click', (e) => {
+    if (!e.target.querySelector('.modal-dialog')) return;
+    modalBlock.classList.remove('d-block');
+    burgerBtn.classList.remove('active');
   });
 
   const playTest = () => {
     const renderQuestions = () => {
+      const burgerName = 'Стандарт';
+      const burgerImageSrc = './image/burger.png';
+
       questionTitle.textContent = 'Какого цвета бургер вы хотите?';
       formAnswers.innerHTML = `
         <div class="answers-item d-flex flex-column">
